593c0875288722bedd30ce5a52d79e982be4be82
Sandip Bhattacharya <sandipb@sandipb.net>
2018-08-09 01:42:22 -0700

    Use vgo+argparse+fatih's color

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sandipb/mmm
+
+require (
+	github.com/akamensky/argparse v0.0.0-20180801024819-0d55ac017eb9
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"flag"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/akamensky/argparse"
+
+	"github.com/fatih/color"
 )
 
 type stringArray []string
@@ -18,6 +22,18 @@ func (slPtr *stringArray) Set(value string) error {
 	return nil
 }
 
+func printError(format string, params ...interface{}) {
+	fmt.Fprintf(os.Stderr,
+		"%s "+format+"\n",
+		append([]interface{}{color.RedString("!!!")}, params...)...)
+}
+
+func printInfo(format string, params ...interface{}) {
+	fmt.Fprintf(os.Stderr,
+		"%s "+format+"\n",
+		append([]interface{}{color.CyanString("***")}, params...)...)
+}
+
 func usage() {
 	fmt.Fprintln(os.Stderr, "USAGE: mmm SRC_DIR TYPE_A=DST_DIR_A TYPE_B=DST_DIR_B ...")
 	fmt.Fprintln(os.Stderr, "")
@@ -26,31 +42,35 @@ func usage() {
 
 func validateDir(path string) {
 	if fi, err := os.Stat(path); err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		printError("%v", err)
 		os.Exit(1)
 	} else if !fi.IsDir() {
-		fmt.Fprintf(os.Stderr, "ERROR: %s is not a directory\n", path)
+		printError("%s is not a directory", path)
 		os.Exit(1)
 	}
 }
 
 func main() {
 
-	var sources, dests stringArray
-
-	flag.Var(&sources, "src", "Source directory `path`. Can be repeated.")
-	flag.Var(&dests, "dst", "Map of mime type to  destination directory in the form `type=dir`. Can be repeated.")
-	flag.Parse()
-
+	parser := argparse.NewParser("mmm", "Distributes file based on mime type")
+	sources := parser.List("s", "src",
+		&argparse.Options{Required: true, Help: "Source directory"})
+	dests := parser.List("d", "dst",
+		&argparse.Options{Required: true, Help: "Map of mime type to  destination directory in the form `type=dir`"})
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(errors.New(color.RedString("!!! ") + err.Error())))
+		return
+	}
 	mimeMap := map[string]string{}
 
-	for _, dir := range sources {
+	for _, dir := range *sources {
 		validateDir(dir)
 	}
 
-	for _, m := range dests {
+	for _, m := range *dests {
 		if !strings.Contains(m, "=") {
-			fmt.Fprintf(os.Stderr, "ERROR: Invalid destination mapping %v. See help.\n", m)
+			printError("Invalid destination mapping %v. See help.\n", m)
 			os.Exit(1)
 		}
 		fields := strings.SplitN(m, "=", 2)
@@ -58,18 +78,18 @@ func main() {
 		mimeMap[fields[0]] = fields[1]
 	}
 
-	if len(sources) == 0 {
-		fmt.Fprintln(os.Stderr, "ERROR: No sources specified")
+	if len(*sources) == 0 {
+		printError("No sources specified")
 		os.Exit(1)
 	}
 
 	if len(mimeMap) == 0 {
-		fmt.Fprintln(os.Stderr, "ERROR: No destinations specified")
+		printError("No destinations specified")
 		os.Exit(1)
 	}
 
-	fmt.Printf("*** Will read files from directories: %v\n", sources)
+	printInfo("Will read files from directories: %v", *sources)
 	for t, d := range mimeMap {
-		fmt.Printf("*** ... and copy files of type %s/* to: %s\n", t, d)
+		printInfo("... and copy files of type %s/* to: %s", t, d)
 	}
 }
